255d210cdf51c6a89d8172c24e814b2d4531a811
Johan Villamizar <johan_1115@hotmail.com>
2024-09-25 14:34:04 -0500

    creacion del front- aun falta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { UsuariosService } from './usuarios.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [UsuariosService]
 })
 export class AppComponent {
-  title = 'front-tienda';
+  posts: any[] = []
+  constructor(private usuario:UsuariosService){
+    this.usuario.getPost().subscribe(post => {
+      console.log(post)
+      this.posts = [post]
+    })
+  }
+
+  title = 'usuarios de la tienda';
+
+}
+
+export interface usuarios{
+  id:number;
+  nombre:String
+  correo:String
 }
